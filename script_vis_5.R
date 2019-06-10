8266d175ebc64fde54a92d13616f58849317dae1
ajstewartlang <andrew.stewart@manchester.ac.uk>
2019-06-10 17:22:45 +0100

    first commit

---
+++ script_vis_5.R
@@ -0,0 +1,25 @@
+library(ggplot2)
+library(ggthemes)
+library(readr)
+library(dplyr)
+library(stringr)
+library(tools)
+
+full_trains <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-26/full_trains.csv")
+
+# Visualisation 5 ####
+full_trains %>%
+  filter(year == 2017) %>%
+  filter(service == "International") %>%
+  mutate(departure_station = toTitleCase(tolower(departure_station))) %>%
+  mutate(departure_station = reorder(departure_station, num_late_at_departure)) %>%
+  mutate(month = factor(month, levels = 1:12, labels = month.name)) %>%
+  ggplot(aes(x = departure_station, y = num_late_at_departure, 
+             colour = departure_station)) +
+  geom_boxplot() +
+  guides(colour = FALSE) +
+  coord_flip() +
+  labs(title = "International SNCF Trains Departing Late in 2017",
+       subtitle = "By Departure Station")  +
+  scale_color_colorblind() +
+  theme_fivethirtyeight() 
